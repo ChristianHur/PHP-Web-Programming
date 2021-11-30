1db62156d03bd6d29ae9f508928582b79a70a224
Christian Hur <chrisvhur@gmail.com>
2021-11-30 12:12:15 -0600

    Add files via upload

---
+++ unit1/assignment1/myfirstphp.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First PHP Page</title>
+</head>
+<body>
+<h1><?php
+    echo "This is my first PHP page!";
+    ?></h1>
+<p>
+    <?php
+    echo "by Christian Hur";
+    ?>
+</p>
+<footer>
+    <?php
+    echo "&copy;2020 Christian Hur.  PHP Web Programming."
+    ?>
+</footer>
+</body>
+</html>
+
